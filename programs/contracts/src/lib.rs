e07b6e0942546bdb4253715580c3a27e74839030
Digvijay Singh Rathore <digvijaysra@gmail.com>
2022-01-02 02:55:38 +0530

    prolly program v1 smth

--- programs/contracts/src/lib.rs
+++ programs/contracts/src/lib.rs
@@ -6,41 +6,49 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod splits_program {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey, splits: Vec<Splits>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey, addresses: Vec<Pubkey>, percentages: Vec<u64>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         let mut total_percentage = 0;
+        let mut index = 0;
 
-        for split in splits.iter() {
-            total_percentage = total_percentage + split.percentage
+        for ac_address in addresses.iter() {
+            total_percentage = total_percentage + percentages[index];
+            index = index + 1;
         }
 
         assert_eq!(total_percentage, 100, "INIT:: total percentage should be 100");
 
         base_account.authority = authority;
-        base_account.splits = splits;
+        base_account.addresses = addresses;
+        base_account.percentages = percentages;
 
         Ok(())
     }
 
-    pub fn send_sol(ctx: Context<SenderContext>, amount: u64) -> ProgramResult {
+    pub fn send_sol<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, SenderContext<'info>>, amount: u64) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        let msg_sender = ctx.accounts.msg_sender.key();
+        let msg_sender = &mut ctx.accounts.msg_sender;
+        let mut index = 0;
 
-        for split in base_account.splits.iter() {
-            let split_percentage = Percentage::from(split.percentage);
+        for ac_address in base_account.addresses.iter() {
+            let split_percentage = Percentage::from(base_account.percentages[index]);
             let split_amount = split_percentage.apply_to(amount);
 
-            // let ix = anchor_lang::solana_program::system_instruction::transfer(
-            //     &msg_sender,
-            //     &split.address,
-            //     split_amount,
-            // );
-            
-            anchor_lang::solana_program::system_instruction::transfer(
-                &msg_sender,
-                &split.address,
+            let ix = anchor_lang::solana_program::system_instruction::transfer(
+                &msg_sender.key(),
+                &ac_address,
                 split_amount,
             );
+            
+            anchor_lang::solana_program::program::invoke(
+                &ix,
+                &[
+                    msg_sender.to_account_info(),
+                    ctx.remaining_accounts[index].to_account_info(),
+                ],
+            );
+
+            index = index + 1;
         }
 
         Ok(())
@@ -56,20 +64,22 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
-#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct Splits {
-    pub address: Pubkey,
-    pub percentage: u64,
-}
-
 #[account]
 pub struct BaseAccount {
     pub authority: Pubkey,
-    pub splits: Vec<Splits>
+    pub addresses: Vec<Pubkey>,
+    pub percentages: Vec<u64>
 }
 
 #[derive(Accounts)]
 pub struct SenderContext<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    pub msg_sender: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ReaderContext<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
     pub msg_sender: Signer<'info>,
